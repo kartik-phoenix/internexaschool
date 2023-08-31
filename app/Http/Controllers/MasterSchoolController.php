e1bcbcb6fe1cc8e927dfed3fc0b105061b28edda
Dipak Dhola <dipak@ip-172-26-8-140.ap-south-1.compute.internal>
2023-08-31 14:05:59 +0530

    delete

--- app/Http/Controllers/MasterSchoolController.php
+++ app/Http/Controllers/MasterSchoolController.php
@@ -129,7 +129,7 @@ class MasterSchoolController extends Controller
 
             if (!empty($database)) {
                 shell_exec('mysqladmin -u ' . env('DB_USERNAME') . ' -p' . env('DB_PASSWORD') . ' create ' . $database . '');
-                shell_exec('mysql -u ' . env('DB_USERNAME') . ' -p' . env('DB_PASSWORD') . ' ' . $database . ' < /' . dirname(base_path()) . '/internexaschool_clone/database/internexaschool.sql');
+                shell_exec('mysql -u ' . env('DB_USERNAME') . ' -p' . env('DB_PASSWORD') . ' ' . $database . ' < ' . $sourceFolder . '/database/internexaschool.sql');
 
                 $envFile = $destinationFolder . '/.env';
 
